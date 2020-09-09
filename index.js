a5debf87ec42d975fdbc293b54dcf57b7b4dc258
Brandon Raffa <branaffa@udel.edu>
2020-09-09 19:06:47 -0400

    finished activity

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function () {
     //this code runs after page is fully loaded
+    $("#nameform").on('submit', function (e) {
+        e.preventDefault();
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var newHTML = '<ch1>' + fname + ' ' + lname + '</ch1>';
+        $(".jumbotron").removeClass('d-none').html(newHTML);
+        //$("#j3").html("<ch3>test</ch3>");
+        //console.log(html);
+        return false; //prevents reload on submit
     });
+});
